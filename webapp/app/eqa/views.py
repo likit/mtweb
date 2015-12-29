47a865297fac8d54f44fcfe22a2c7a02ffa777cd
Likit Preeyanon <preeyano@msu.edu>
2015-12-29 21:18:11 +0700

    added the result view

--- webapp/app/eqa/views.py
+++ webapp/app/eqa/views.py
@@ -1,15 +1,22 @@
-from flask import Blueprint, render_template
-from flask.ext.login import login_required
+from flask import Blueprint, render_template, flash, redirect, url_for
+from flask.ext.login import login_required, current_user
 from .forms import ResultForm
+from app.models import LabInfo
 
 eqa = Blueprint('eqa', __name__, template_folder='templates')
 
 @eqa.route('/', methods=['GET'])
+# @login_required
 def index():
-    return render_template('eqa/index.html')
+    if current_user.is_authenticated:
+        if current_user.labinfo:
+            return redirect(url_for('eqa.results'))
+        else:
+            flash('You need to add lab information to proceed.')
+            return redirect(url_for('customers.add_lab_info'))
 
-@eqa.route('/customer/results', methods=['GET'])
-@login_required
-def customer_results():
+@eqa.route('/results', methods=['GET'])
+# @login_required
+def results():
     form = ResultForm()
     return render_template('eqa/results.html', form=form)
