e643295021eff3f732d3640defd6ade97e55b519
Likit Preeyanon <preeyano@msu.edu>
2015-12-30 11:24:12 +0700

    added config file

---
+++ webapp/app/config.py
@@ -0,0 +1,29 @@
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
+class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'thegodfather'
+    SQLALCHEMY_COMMIT_ON_TEARDOWN = True
+
+    @staticmethod
+    def init_app(app):
+        pass
+
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or \
+            'sqlite:///' + os.path.join(basedir, '../data-dev.db')
+
+
+class ProductionConfig(Config):
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+            'sqlite:///' + os.path.join(basedir, '../mtweb.db')
+
+
+config = {
+        'development': DevelopmentConfig,
+        'production': ProductionConfig,
+        'default': DevelopmentConfig
+        }
