f43aa3ff4a12e522a0a4ad5f2a90821bad557bbe
Likit Preeyanon <preeyano@msu.edu>
2016-04-17 02:01:11 +0700

    added a function to check a user type

--- webapp/app/decorators.py
+++ webapp/app/decorators.py
@@ -1,7 +1,7 @@
 from functools import wraps
 from flask import abort
 from flask.ext.login import current_user
-from app.models import Permission
+from app.models import Permission, UserType
 
 
 def permission_required(permission):
@@ -17,3 +17,18 @@ def permission_required(permission):
 
 def admin_required(f):
     return permission_required(Permission.ADMINISTER)(f)
+
+
+def correct_user_type_required(utype):
+    def decorator(f):
+        @wraps(f)
+        def decorated_function(*args, **kwargs):
+            if not current_user.correct_user_type(utype):
+                abort(403)
+            return f(*args, **kwargs)
+        return decorated_function
+    return decorator
+
+
+def student_required(f):
+    return correct_user_type_required(UserType.STUDENT)(f)
