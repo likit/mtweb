b9bb67f08eccc1560e9515361a2ffb9e34b54819
Likit Preeyanon <preeyano@msu.edu>
2016-04-17 02:00:04 +0700

    made permissions available in all template via a context processor

--- webapp/app/main/views.py
+++ webapp/app/main/views.py
@@ -1,7 +1,14 @@
 from flask import Blueprint, render_template
+from app.models import Permission
 
 main = Blueprint('main', __name__, template_folder='templates')
 
 @main.route('/', methods=['GET'])
 def index():
     return render_template('main/index.html')
+
+
+# make permission available in all templates
+@main.app_context_processor
+def inject_permission():
+    return dict(Permission=Permission)
